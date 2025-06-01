18e39231e1d5292b7c9c3571e563d2449a75bc3d
Morpho2005 <digitalchristian05@gmail.com>
2025-06-01 17:34:06 -0600

    added base code

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    
+    res.send('Welcome to the School Management Api');
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
+
