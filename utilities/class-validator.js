4d9d6bed9af5cd4f2353bf4d1e6f74a30740bf7b
Morpho2005 <digitalchristian05@gmail.com>
2025-06-14 17:25:39 -0600

    fixed class validator

--- utilities/class-validator.js
+++ utilities/class-validator.js
@@ -27,24 +27,24 @@ validate.classValidationRules = () => {
         body('room')
             .notEmpty()
             .withMessage('Room is required')
-            .trim()
             .custom(value => {
                 const pattern = new RegExp("Block [A-Z] - Room [0-9{2}]")
                 if (!pattern.test(value)) {
-                    throw new Error("Room must be in format 'Block y - Room x' where y is any capital letter and x is any number with 2 or less digits");
+                    throw new Error("Room must be in format 'Block y - Room x' where y is any capital letter and x is any number with 2 digits");
                 }
-            }),
+            })
+            .trim(),
 
         body('schedule')
             .notEmpty()
             .withMessage('Schedule is required')
             .custom(value => {
-                const pattern = new RegExp("[A-Za-z{3}] [0-9{2}]:[0-9{2}] - [0-9{2}]:[0-9{2}]")
+                const pattern = new RegExp("[0-9{2}]:[0-9{2}] - [0-9{2}]:[0-9{2}]")
                 if (!pattern.test(value)) {
                     throw new Error("Room must be in format \"misc x:xx - x:xx\" ");
                 }
             })
-
+            .trim(),
     ]
 }
 
